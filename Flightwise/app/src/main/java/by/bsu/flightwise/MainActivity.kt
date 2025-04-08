b8d366b08a6f2007b69a334fc6676a6600d7f7f9
tayadj <tayadj@users.noreply.github.com>
2025-04-08 21:50:39 +0300

    feat: Added header, footer, theme scheme

--- Flightwise/app/src/main/java/by/bsu/flightwise/MainActivity.kt
+++ Flightwise/app/src/main/java/by/bsu/flightwise/MainActivity.kt
@@ -5,16 +5,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester.Companion.createRefs
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import by.bsu.flightwise.ui.theme.FlightwiseTheme
-import by.bsu.flightwise.ui.theme.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,45 +38,95 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun FlightwiseApp() {
-    Column(
-        modifier = Modifier.fillMaxSize().padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        // Section 1
-        Text(
-            text = "Discover countries",
-            style = MaterialTheme.typography.headlineLarge.copy(color = Highlight)
-        )
-        Text(
-            text = "To choose a ticket, you can firstly discover countries here",
-            style = MaterialTheme.typography.bodySmall.copy(color = Black)
-        )
-        Button(
-            onClick = { navigateToCountriesActivity() },
-            modifier = Modifier.padding(top = 16.dp),
-            // colors = buttonColors(backgroundColor = Highlight)
+    Box(modifier = Modifier.fillMaxSize()) {
+        // Header
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.primary)
+                .padding(16.dp)
+                .align(Alignment.TopCenter)
         ) {
-            Text(text = "Find out more", color = White)
+            Text(
+                text = "Flightwise",
+                color = MaterialTheme.colorScheme.onPrimary,
+                style = MaterialTheme.typography.headlineLarge
+            )
         }
 
-        Spacer(modifier = Modifier.height(32.dp))
-
-        // Section 2
-        Text(
-            text = "Ready to go",
-            style = MaterialTheme.typography.headlineLarge.copy(color = Highlight)
-        )
-        Text(
-            text = "If you know your choice, you can go ahead and buy a ticket!",
-            style = MaterialTheme.typography.bodySmall.copy(color = Black)
-        )
-        Button(
-            onClick = { navigateToSearchTicketsActivity() },
-            modifier = Modifier.padding(top = 16.dp),
-            // colors = ButtonDefaults.buttonColors(backgroundColor = Highlight)
+        // Footer
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.primary)
+                .padding(16.dp)
+                .align(Alignment.BottomCenter),
+            contentAlignment = Alignment.Center
         ) {
-            Text(text = "Buy a ticket", color = White)
+            Text(
+                text = "© 2025 Flightwise",
+                color = MaterialTheme.colorScheme.onPrimary,
+                style = MaterialTheme.typography.labelSmall
+            )
+        }
+
+        // Main Content
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 16.dp)
+                .padding(top = 96.dp, bottom = 64.dp)
+                .align(Alignment.Center),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            // Display Image
+            Image(
+                painter = painterResource(id = R.drawable.main_activity_header),
+                contentDescription = "Main Activity Header",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(1.5f)
+            )
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            // Section 1
+            Text(
+                text = "Discover countries",
+                color = MaterialTheme.colorScheme.primary,
+                style = MaterialTheme.typography.headlineMedium
+            )
+            Text(
+                text = "To choose a ticket, you can firstly discover countries here",
+                color = MaterialTheme.colorScheme.onBackground,
+                style = MaterialTheme.typography.labelSmall
+            )
+            Button(
+                onClick = { navigateToCountriesActivity() },
+                modifier = Modifier.padding(top = 16.dp)
+            ) {
+                Text(text = "Find out more", color = MaterialTheme.colorScheme.onPrimary)
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // Section 2
+            Text(
+                text = "Ready to go",
+                color = MaterialTheme.colorScheme.primary,
+                style = MaterialTheme.typography.headlineMedium
+            )
+            Text(
+                text = "If you know your choice, you can go ahead and buy a ticket!",
+                color = MaterialTheme.colorScheme.onBackground,
+                style = MaterialTheme.typography.labelSmall
+            )
+            Button(
+                onClick = { navigateToSearchTicketsActivity() },
+                modifier = Modifier.padding(top = 16.dp)
+            ) {
+                Text(text = "Buy a ticket", color = MaterialTheme.colorScheme.onPrimary)
+            }
         }
     }
 }
