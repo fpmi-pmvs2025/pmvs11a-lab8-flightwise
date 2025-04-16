e7aaff495e5ed74b2b3e889d6bdf5b19277f49f3
tayadj <tayadj@users.noreply.github.com>
2025-04-16 11:40:27 +0300

    feat: Flight entity parcelized

--- Flightwise/app/src/main/java/by/bsu/flightwise/data/entity/Flight.kt
+++ Flightwise/app/src/main/java/by/bsu/flightwise/data/entity/Flight.kt
@@ -1,13 +1,17 @@
 package by.bsu.flightwise.data.entity
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import kotlinx.parcelize.RawValue
 import java.util.Date
 
+@Parcelize
 data class Flight(
-    val id: Long = 0,
+    val id: Long,
     val airplaneId: Long,
     val fromAirportId: Long,
     val toAirportId: Long,
-    val pricingRuleId: Long,
-    val departureTime: Date,
-    val arrivalTime: Date
-) 
+    val departureTime: @RawValue Date,
+    val arrivalTime: @RawValue Date,
+    val pricingRuleId: Long
+) : Parcelable
